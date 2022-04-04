fc6232c09b758836f1a669e3483c8140d8970412
unknown <chahine.benlahcen@gmail.com>
2022-04-04 18:23:39 +0200

    setup project

---
+++ client/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Image from 'next/image'
+import Card from "../components/Card"
+
+export default function Home() {
+  return (
+    <div className="">
+      <Card/>
+    </div>
+
+  )
+}
